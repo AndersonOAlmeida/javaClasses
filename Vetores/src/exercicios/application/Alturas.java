ee12f918ffebecd3eac41eec3f140998018288eb
Anderson Almeida <anderson.almeida@doutoresdaweb.com.br>
2026-08-03 18:01:36 -0300

    exercicio altura e classe pessoa

---
+++ Vetores/src/exercicios/application/Alturas.java
@@ -0,0 +1,21 @@
+package exercicios.application;
+
+/* Importação das classes utilizadas no programa */
+import java.util.Locale;
+import java.util.Scanner;
+import exercicios.entities.Person;
+
+public class Alturas {
+    public static void main(String[] args) {
+        /* Configurando a formatação dos dados inseridos para o estilo americano */
+        Locale.setDefault(Locale.US);
+
+        /* Criando uma classe para o usuário inserir as informações */
+        Scanner sc = new Scanner(System.in);
+
+        /* Criando um objeto pessoa baseado na classe Person importada */
+        Person pessoa = new Person();
+
+        /*  */
+    }
+}
---
+++ Vetores/src/exercicios/entities/Person.java
@@ -0,0 +1,40 @@
+package exercicios.entities;
+
+public class Person {
+    private String name;
+    private int age;
+    private double height;
+
+    /* Getters */
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    /* Setters */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    /* Construtores */
+    public void Person(String name, int age, double height) {
+        setName(name);
+        setAge(age);
+        setHeight(height);
+    }
+}
