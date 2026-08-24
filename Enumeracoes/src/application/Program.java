3d540e25833194c45a41cedb43f411e234a2c1e8
Anderson Almeida <anderson.almeida@doutoresdaweb.com.br>
2026-08-24 10:37:44 -0300

    notas sobre enumerações

--- Enumeracoes/src/application/Program.java
+++ Enumeracoes/src/application/Program.java
@@ -7,6 +7,9 @@ import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
+        /* Enumerações */
+        /* Definição: enumeração é um tipo que serve para especificar de forma literal um conjunto de constantes relacionadas, usada
+        * com a palavra-chave enum. A vantagem de uma enumeração é a legibilidade, a melhor semântica e o auxílio do compilador */
         Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
 
         System.out.println(order);
--- Enumeracoes/src/entities/enums/OrderStatus.java
+++ Enumeracoes/src/entities/enums/OrderStatus.java
@@ -1,4 +1,4 @@
-package entities.enums;
+package entities.enums; // para melhor organização, é bom colocar os enums como dependências das entities no projeto
 
 public enum OrderStatus {
     PENDING_PAYMENT,
